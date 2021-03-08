9fa67e3676c1f892fc41b128dff6f4eabac5d877
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 07:32:28 +0700

    Update Luas Segitiga.cpp

--- Ekspresi dan Masukan/Luas Segitiga.cpp
+++ Ekspresi dan Masukan/Luas Segitiga.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main(){
-	int alas, tinggi;
-	float hasil;
-	cin >> alas >> tinggi;
-	cout << 0.5*((float)alas*(float)tinggi);
-	printf("%.2f", hasil);
-	return 0;
+    int alas, tinggi;
+    float hasil;
+    cin >> alas >> tinggi;
+    cout << 0.5*((float)alas*(float)tinggi);
+    printf("%.2f", hasil);
+    return 0;
 }
