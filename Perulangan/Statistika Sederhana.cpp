7abd2058626ec9bfdc573f085e9e7edaeaa95389
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 08:26:11 +0700

    Update Statistika Sederhana.cpp

--- Perulangan/Statistika Sederhana.cpp
+++ Perulangan/Statistika Sederhana.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 
 int main(){
- 	int bil, total, trbesar, trkecil, rata, i, n;
- 	total = 0;
- 	cin >> n;
- 	for(i=1; i<=n; i++){
-		scanf("%d", &bil);
-  			total=total+bil;
- 		 if(i==1){
-   			trbesar=bil;
-   			trkecil=bil;
-  			}
-  		 else if(trkecil>bil){
-   		 	trkecil=bil;
-  			}
-   		 else if (trbesar<bil){
-   		    trbesar=bil;
-   			}
-   		}
- 	cout << trbesar << " " << trkecil;
-	return 0;
+    int bil, total, trbesar, trkecil, rata, i, n;
+    total = 0;
+    cin >> n;
+    for(i=1; i<=n; i++){
+        cin >> bil;
+  	total=total+bil;
+        if(i==1){
+            trbesar=bil;
+            trkecil=bil;
+        }
+        else if(trkecil>bil){
+            trkecil=bil;
+        }
+        else if (trbesar<bil){
+            trbesar=bil;
+        }
+    }
+    cout << trbesar << " " << trkecil;
+    return 0;
 }
