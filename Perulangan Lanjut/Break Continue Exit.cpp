3703578af03f6ffc2c3feadce72d921e9484b7d3
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 11:40:47 +0700

    Create Break Continue Exit.cpp

---
+++ Perulangan Lanjut/Break Continue Exit.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int x, i;
+	cin >> x;
+	for(i=1; i<=x; i++){
+		if(i%10 == 0)
+			continue;
+		if(i>= 42){
+			cout << "ERROR";
+			break;
+		}
+		cout << i << endl;
+	}
+	return 0;
+}
