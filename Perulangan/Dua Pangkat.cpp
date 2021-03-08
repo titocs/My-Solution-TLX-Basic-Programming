1d96e451edb34af63171c0666ec142ca61e9fc93
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 08:16:59 +0700

    Update Dua Pangkat.cpp

--- Perulangan/Dua Pangkat.cpp
+++ Perulangan/Dua Pangkat.cpp
@@ -5,19 +5,16 @@ int checkPowerofTwo(int n);
 int main(){
    	int num;
     cin >> num;
-
 	if (checkPowerofTwo(num) == 1)
 		cout << "ya" << endl;
-   else
+    else
       	cout << "bukan" << endl;
-
    return 0;
 }
 
 int checkPowerofTwo(int x){
   	if (x == 0)
      	return 0;
-	
 	while( x != 1){
 		if(x % 2 != 0)
         	return 0;
