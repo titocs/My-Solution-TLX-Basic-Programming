3d5fb95f1092d1acb0af1583e99bfcd17b09ffba
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 08:27:48 +0700

    Update While + Pencacah.cpp

--- Perulangan/While + Pencacah.cpp
+++ Perulangan/While + Pencacah.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(){
-	int n;
-	int hasil=0;
-	while (cin>>n) {
-		hasil=hasil+n;
-	}	
-	cout<<hasil<<endl;
-	return 0;
+    int n;
+    int hasil=0;
+    while (cin>>n) {
+        hasil=hasil+n;
+    }	
+    cout<<hasil<<endl;
+    return 0;
 }
