333ae01bbaaf2a59f355c9fde70841e70fd449f1
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 08:20:58 +0700

    Update Faktor Bilangan.cpp

--- Perulangan/Faktor Bilangan.cpp
+++ Perulangan/Faktor Bilangan.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 
 int main(){
-	int n, i;
-	scanf("%d", &n);
-	
-	for(i=n; i>0; i--){
-		if(n%i ==0 )
-			cout << i << endl;
-	}
-	return 0;
+    int n, i;
+    cin >> n;
+    for(i=n; i>0; i--){
+        if(n%i ==0 )
+            cout << i << endl;
+    }
+    return 0;
 }
