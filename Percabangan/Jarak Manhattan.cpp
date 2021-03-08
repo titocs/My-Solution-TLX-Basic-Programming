90a3a56753b5f9935708d5ad8a9cd352f79e0238
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 07:36:55 +0700

    Update Jarak Manhattan.cpp

--- Percabangan/Jarak Manhattan.cpp
+++ Percabangan/Jarak Manhattan.cpp
@@ -2,11 +2,9 @@
 using namespace std;
 
 int main(){	
-	int hasil;
-	int a, b, c, d;
-	cin >> a >> b >> c >> d;
-	hasil = abs(a-c) + abs(b-d);
-	cout << hasil;
-	
-	return 0;
+    int hasil;
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << abs(a-c) + abs(b-d) << endl;
+    return 0;
 }
