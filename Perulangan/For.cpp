1fef941310ad83281162ab3715a05a1c93d5db4d
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 08:04:24 +0700

    Create For.cpp

---
+++ Perulangan/For.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int bn, i, jml, n;
+ 	cin >> bn;
+  	jml = 0;
+  	for(i=1; i<=bn; i++){
+   		cin >> n;
+  		jml = jml + n;
+ 	}
+ 	cout << jml << endl;
+ 	return 0;
+}
