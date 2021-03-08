3f7163708e6cb3cf1d9cca9378b43bd23c90453c
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 08:23:11 +0700

    Update Pola I.cpp

--- Perulangan/Pola I.cpp
+++ Perulangan/Pola I.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main(){
-	int N, K, i;
-	cin >> N >> K;
-	for ( int i=1; i<=N; i++){
-		if ( (i%K) == 0 ){
-			cout << "*" << " ";
-		}
-		else {
-			cout << i << " ";
-		}
-	}
-	return 0;
+    int N, K;
+    cin >> N >> K;
+    for( int i=1; i<=N; i++){
+        if ( (i%K) == 0 ){
+            cout << "*" << " ";
+        }
+        else {
+            cout << i << " ";
+        }
+    }
+    return 0;
 }
