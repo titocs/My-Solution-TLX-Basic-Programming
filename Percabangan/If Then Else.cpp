da7fe920204d48984e547648bc1a3612fa906b12
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 07:35:21 +0700

    Update If Then Else.cpp

--- Percabangan/If Then Else.cpp
+++ Percabangan/If Then Else.cpp
@@ -2,14 +2,13 @@
 using namespace std;
 
 int main(){
-	int x, y;
-	cin >> x;
-	if(x>0)
-		cout << "positif" << endl;
-	else if(x<0)
-		cout << "negatif" << endl;
-	else
-		cout << "nol" << endl;
-	
-	return 0;
+    int x, y;
+    cin >> x;
+    if(x>0)
+        cout << "positif" << endl;
+    else if(x<0)
+	cout << "negatif" << endl;
+    else
+	cout << "nol" << endl;
+    return 0;
 }
