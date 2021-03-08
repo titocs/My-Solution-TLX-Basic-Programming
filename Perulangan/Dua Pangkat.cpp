7c26e25b04f62aecd15bffcb34f8c3621f1295e6
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-08 08:19:59 +0700

    Update Dua Pangkat.cpp

--- Perulangan/Dua Pangkat.cpp
+++ Perulangan/Dua Pangkat.cpp
@@ -3,22 +3,22 @@ using namespace std;
 
 int checkPowerofTwo(int n);
 int main(){
-   	int num;
+    int num;
     cin >> num;
 	if (checkPowerofTwo(num) == 1)
-		cout << "ya" << endl;
-    else
-      	cout << "bukan" << endl;
+            cout << "ya" << endl;
+        else
+      	    cout << "bukan" << endl;
    return 0;
 }
 
 int checkPowerofTwo(int x){
-  	if (x == 0)
-     	return 0;
-	while( x != 1){
-		if(x % 2 != 0)
-        	return 0;
-         x /= 2;
-   	}
-   	return 1;
+    if (x == 0)
+        return 0;
+    while( x != 1){
+        if(x % 2 != 0)
+            return 0;
+        x /= 2;
+    }
+    return 1;
 }
