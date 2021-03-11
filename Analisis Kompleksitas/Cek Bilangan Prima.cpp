9045b2bfaa6013256ab0abd858952e9748eaacb1
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-11 12:17:38 +0700

    Update Cek Bilangan Prima.cpp

--- Analisis Kompleksitas/Cek Bilangan Prima.cpp
+++ Analisis Kompleksitas/Cek Bilangan Prima.cpp
@@ -2,20 +2,20 @@
 using namespace std;;
 
 int main(){
-	int t; cin>>t;
-	while (t--){
-		int n;
-		cin>>n;
-		bool ada = false;
-		for(int i=2; i*i<=n; i++){
-			if (n%i == 0){
+    int t; cin>>t;
+    while (t--){
+        int n;
+        cin>>n;
+        bool ada = false;
+        for(int i=2; i*i<=n; i++){
+            if (n%i == 0){
 				ada=true;
 			}
 		}
 		if (!ada and n!=1)
-            cout<<"YA\n";
+			cout<<"YA\n";
 		else
-            cout<<"BUKAN\n";
+			cout<<"BUKAN\n";
 	}
 	return 0;
 }
