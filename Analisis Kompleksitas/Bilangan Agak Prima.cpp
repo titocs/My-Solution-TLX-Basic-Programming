779097996f432cf0eef61a3dfb7159b85b2a7501
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-21 07:11:06 +0700

    Create Bilangan Agak Prima.cpp

---
+++ Analisis Kompleksitas/Bilangan Agak Prima.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n, x, cek=0;
+	cin >> n;
+	for(int i=1; i<=n; ++i){
+		cin >> x;
+		int cek=0;
+		for(int j=2; j<x; ++j){
+			if(cek > 2)
+				break;
+			if(x % j == 0)
+				cek++;
+		}
+		if(cek <= 2)
+			cout << "YA" << "\n";
+		else
+			cout << "BUKAN" << "\n";
+	}
+}
