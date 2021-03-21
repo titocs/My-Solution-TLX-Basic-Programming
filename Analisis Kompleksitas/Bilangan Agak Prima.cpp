d3ad864f3c623a7ffcf0f2a22d45d28676fd4bb1
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-21 07:12:41 +0700

    Update Bilangan Agak Prima.cpp

--- Analisis Kompleksitas/Bilangan Agak Prima.cpp
+++ Analisis Kompleksitas/Bilangan Agak Prima.cpp
@@ -2,20 +2,20 @@
 using namespace std;
 
 int main(){
-	int n, x, cek=0;
-	cin >> n;
-	for(int i=1; i<=n; ++i){
-		cin >> x;
-		int cek=0;
-		for(int j=2; j<x; ++j){
-			if(cek > 2)
-				break;
-			if(x % j == 0)
-				cek++;
-		}
-		if(cek <= 2)
-			cout << "YA" << "\n";
-		else
-			cout << "BUKAN" << "\n";
+    int n, x, cek=0;
+    cin >> n;
+    for(int i=1; i<=n; ++i){
+        cin >> x;
+        int cek = 0;
+        for(int j=2; j<x; ++j){
+            if(cek > 2)
+                break;
+            if(x % j == 0)
+                cek++;
+        }
+        if(cek <= 2)
+            cout << "YA" << "\n";
+        else
+            cout << "BUKAN" << "\n";
 	}
 }
