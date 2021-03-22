70786df9cd0613e5dcbfaa66d7ed2d3bbf6172f4
Tito Candra Septio <55591161+titocs@users.noreply.github.com>
2021-03-22 10:07:10 +0700

    Create Balik Daftar.cpp

---
+++ Array/Balik Daftar.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[100];
+    int i=0;
+    while (cin >> arr[i]){
+        i++;
+    }
+    i--;
+    while (i>=0){
+        cout << arr[i] << endl;
+        i--;
+    }
+}
